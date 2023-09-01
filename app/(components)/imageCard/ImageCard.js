235204efad03d9c40ed38b1f08b8d0b3815fe012
UMAIR JAMAAT ALI <umairansari758@gmail.com>
2023-09-01 23:18:38 +0500

    practice

---
+++ app/(components)/imageCard/ImageCard.js
@@ -0,0 +1,19 @@
+const ImageCard = () => {
+    return (
+      <>
+        <div className="w-[350px] h-[400px] border-solid border-black mx-4 relative">
+          <img
+            src="/img/10.jpg" // Replace with your image source
+            alt="Image"
+            className="w-full h-full object-cover"
+          />
+          <p className="hidden absolute inset-0 items-center justify-center bg-black bg-opacity-50 text-white text-lg hover:flex transition-opacity duration-300">
+            Our Galaxy
+          </p>
+        </div>
+      </>
+    );
+  };
+  
+  export default ImageCard;
+  
