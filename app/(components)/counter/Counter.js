ca2f605ee1c6d235c03fc4f58cd1718c57ac4154
UMAIR JAMAAT ALI <umairansari758@gmail.com>
2023-09-05 16:41:27 +0500

    counter and condition

---
+++ app/(components)/counter/Counter.js
@@ -0,0 +1,31 @@
+'use client'
+import { useState } from 'react';
+
+const Counter = () => {
+
+    const [count, setCount] = useState(0);
+   
+   
+    const decrementHandler = ()=>{
+        if (count >= 1 ) {
+            setCount(count - 1);
+           } 
+    }
+
+    const incrementHandler = () => {
+        setCount(count + 1);
+    }
+
+
+    return (
+        <div>
+            <button className='w-[110px] h-[50px] bg-slate-400 m-5' onClick={decrementHandler}>-</button>
+            <div className='w-[150px] h-[80px] border-2 border-black text-center pt-5 '>{count}</div>
+            <button className='w-[110px] h-[50px] bg-slate-400 m-5' onClick={incrementHandler}> + </button>
+            {count !== 0 && <button className='w-[110px] h-[50px] bg-slate-400 m-5' onClick={()=> setCount(0)}>reset</button>}
+            
+        </div>
+    );
+}
+
+export default Counter;
--- app/page.js
+++ app/page.js
@@ -1,11 +1,25 @@
+import Counter from "./(components)/counter/Counter";
 import ImageCard from "./(components)/imageCard/ImageCard";
 
 
 export default function Home() {
+  const login = false;
+
   return (
    <>
    <div className='text-amber-600'>Hello World</div>
    <ImageCard/>
+
+   <div className="m-10">
+    { login ? <button>logout</button>
+  :
+    <button>login</button>
+  }
+    
+    <h1 className="m-6 text-xl font-bold">Counter Handler App</h1>
+    <Counter/>
+    
+   </div>
    </>
   )
 }
